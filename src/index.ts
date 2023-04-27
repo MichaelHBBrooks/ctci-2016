012b239f2ab7a2de179dc8d93c0c27fc5f6fa07c
Michael Brooks <michael.h.b.brooks@gmail.com>
2023-04-27 15:56:45 -0400

    Rename chapter directory because I hate it.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
+import { stringRotation } from "./1_arrays_and_strings/1-9-StringRotation";
 main();
 
 function main(): void {
 	console.log("This is a collection of my answers to Cracking the Coding Interview.");
 	console.log('Use "npm run test" to test this application.');
+
+	console.log(stringRotation("abcdef", "defabc"));
+	console.log(stringRotation("abcdef", "abcabc"));
+	console.log(stringRotation("abcdef", "bcdefa"));
+	console.log(stringRotation("abcdef", "fabcde"));
+	console.log(stringRotation("aaaaaa", "abbbba"));
 }
